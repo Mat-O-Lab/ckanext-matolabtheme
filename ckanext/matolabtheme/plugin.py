69010158eedc7ed8831828fa18cc8ba75b7709b7
Thomas Hanke <thomas.hanke@iwm.fraunhofer.de>
2024-04-23 14:17:16 +0200

    changed navbar

--- ckanext/matolabtheme/plugin.py
+++ ckanext/matolabtheme/plugin.py
@@ -1,10 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.matolabtheme import helpers
+
 
 class MatolabthemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    
 
     # IConfigurer
 
@@ -13,4 +14,11 @@ class MatolabthemePlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "matolabtheme")
 
-    
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return {
+            "parent_site_url": helpers.parent_site_url,
+            "modify_geojson": helpers.modify_geojson,
+            "check_ckan_version": toolkit.check_ckan_version,
+        }
