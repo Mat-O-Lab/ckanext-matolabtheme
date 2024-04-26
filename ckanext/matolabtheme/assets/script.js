e1748f878086535a1376cc2be4fb611ffdb46058
Thomas Hanke <thomas.hanke@iwm.fraunhofer.de>
2024-04-26 07:32:03 +0200

    added a counter animation

--- ckanext/matolabtheme/assets/script.js
+++ ckanext/matolabtheme/assets/script.js
@@ -4,7 +4,21 @@ ckan.module("matolabtheme-module", function ($, _) {
     options: {
       debug: false,
     },
-
-    initialize: function () {},
+    initialize: function () {
+      const counters = document.querySelectorAll(".theme-counter");
+      counters.forEach((counter) => {
+        counter.innerText = "0";
+        const updateCounter = () => {
+          const target = +counter.getAttribute("data-target");
+          const count = +counter.innerText;
+          const increment = target / 200;
+          if (count < target) {
+            counter.innerText = `${Math.ceil(count + increment)}`;
+            setTimeout(updateCounter, 1);
+          } else counter.innerText = target;
+        };
+        updateCounter();
+      });
+    },
   };
 });
